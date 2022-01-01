85404611798a705062725087175bd7f625eb154d
hibuz <hibuz@hibuz.com>
2022-01-01 09:35:06 +0900

    feat: hadoop history server

--- hadoop/hive/docker-entrypoint.sh
+++ hadoop/hive/docker-entrypoint.sh
@@ -6,18 +6,23 @@ if [ ! -d "/tmp/hadoop-`whoami`/dfs/name" ]; then
     hdfs namenode -format
 fi
 
-$HADOOP_HOME/sbin/start-dfs.sh
+start-dfs.sh
 
 if [ ! -d "$HIVE_HOME/metastore_db" ]; then
     init-hive-dfs.sh
     schematool -dbType derby -initSchema
 fi
 
-if [ "$1" == "yarn" ]; then
-$HADOOP_HOME/sbin/start-yarn.sh
+if [ "$1" == *"yarn"* ]; then
+    start-yarn.sh
+fi
+
+if [ "$1" == *"historyserver"* ]; then
+    mapred --daemon start historyserver
 fi
 
 jps
+
 hdfs dfsadmin -report
 
 hiveserver2
